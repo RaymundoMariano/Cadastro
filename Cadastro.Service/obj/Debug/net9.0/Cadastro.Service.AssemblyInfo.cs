8570ec9c362c13d37d714a71003460a5d5370aba
Raymundo Mariano <gledisson@hotmail.com>
2025-02-09 08:30:28 -0300

    Ajustes

--- Cadastro.Service/obj/Debug/net9.0/Cadastro.Service.AssemblyInfo.cs
+++ Cadastro.Service/obj/Debug/net9.0/Cadastro.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cadastro.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b3e7b0ec593b12ef545e5c4efc1e8d5d6a3235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a470bbc75176f2cc4aa71ff9e971c0647bcb80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cadastro.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cadastro.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
